9dcc275fa700db4da171a2827e86cb1f43f390ba
torchhound <unknownmasayoshi@gmail.com>
2016-12-13 21:53:53 -0500

    Padding should be unnecessary but removing it causes the program to not return anything, with the padding the program constantly hits key errors from the lookup tables

--- poker.py
+++ poker.py
@@ -17,24 +17,29 @@ def calcFlopWins(decks, players, trials):
 	Lets the user input a number of decks D, a number of players P, and a number of trials N. Then it plays N rounds of texas hold-em with P players and D decks. Returns the percentage of those N rounds in which the player with the best cards after the flop ended up winning the round. So specifically, for each round, after the flop it finds the player with the best cards, and sets total += 1 if that player wins the round, and total += 0 if that player doesn't win. Returns count / N.
 	"""
 	try:
-		deck = [] #initialize empty deck
 		count = 0 #number of times player with best cards after the flop wins the hand
-		for x in range(decks): #append shuffled decks
-			deck.extend(singleDeck())
-		random.shuffle(deck) #shuffle deck
 		while trials >= 0: #play selected number of hands
+			deck = [] #initialize empty deck
+			for x in range(decks): #append shuffled deck(s)
+				deck.extend(singleDeck())
+			random.shuffle(deck) #shuffle deck
 			flopBest = "" 
 			winner = ""
 			board = []
 			hands = {}
 			for x in range(players): #create hands and add them to dictionary
 				hands[x] = [deck.pop() for __ in range(2)]
-			print(hands)
-			board.append([deck.pop() for __ in range(3)]) #create the flop
+			for __ in range(3):
+				board.append(deck.pop()) #create the flop
+			for __ in range(2): #add padding because evaluate_hand only accepts 2, 5, or 7 arguments
+				board.append(Card(3, 2))
 			for x in range(players): #find the winner after the flop
 				score = 0
+				print(len(hands[x]), len(board))
 				if HandEvaluator.evaluate_hand(hands[x], board) >= score:
 					flopBest = hands[x]
+			for __ in range(2): #remove padding
+				del board[-1]
 			board.append(deck.pop()) #create the turn
 			board.append(deck.pop()) #create the river
 			for x in range(players): #find the winner of the hand
@@ -43,13 +48,13 @@ def calcFlopWins(decks, players, trials):
 					winner = hands[x]
 			if flopBest == winner: #check if the winner of the hand was also on top after the flop
 				count += 1
-		return count / trials 
+		return count / trials
 	except TypeError as e: #catch error from no args present
-		print(e) 
-		return False
-	except Exception as e:
-		print(e)
-		return False
+		print("No args present")
+		pass
+	#except Exception as e:
+	#	print("other error")
+	#	pass
 			
 def main():
 	parser = argparse.ArgumentParser(description=calcFlopWins.__doc__)
